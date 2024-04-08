8d2fab3a5d2bbef95fa7d95e0cf22b2c519674b3
SharonEris <erissharon481@gmail.com>
2024-04-08 16:26:03 +0300

    first

---
+++ CustomIdentity/ViewModels/LoginVm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentity.ViewModels
+{
+    public class LoginVm
+    {
+        [Required(ErrorMessage = "Username is Required")]
+        public string? UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is Required")]
+        public string? Password { get; set; }
+
+        [Display(Name = "RememberMe")]   
+        public bool RememberMe { get; set; }
+    }
+}
